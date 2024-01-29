e9a778155523a9c5232c3b11575a396537ac9e69
viyan <viyanomd@gmail.com>
2024-01-29 17:31:57 +0100

    final. hw 1 task 2. (add decimal dot functionality)

---
+++ Core/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFApplication.Core
+{
+    internal static class Calculator
+    {
+        public static decimal Add(decimal x, decimal y)
+        {
+            return x + y;
+        }
+
+        public static decimal Substract(decimal x, decimal y) 
+        {
+            return x - y;
+        }
+
+        public static decimal Divide(decimal x, decimal y) 
+        {
+            return x / y;
+        }
+
+        public static decimal Multiply(decimal x, decimal y)
+        {
+            return x * y;
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WPFApplication
 {
@@ -16,9 +8,95 @@ namespace WPFApplication
     /// </summary>
     public partial class MainWindow : Window
     {
+        decimal leftNumber = 0;
+        decimal rightNumber = 0;
+        string currentOperation = String.Empty;
+        bool inputStatus = true;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void bt_numClicked(object sender, RoutedEventArgs e)
+        {
+            Button selectedBtn = (Button)sender;
+
+            if (!String.IsNullOrEmpty(currentOperation) && inputStatus)
+            {
+                label_result.Text = String.Empty;
+                inputStatus = false;
+            }
+
+            label_operations.Text += selectedBtn.Content.ToString();
+
+            label_result.Text += selectedBtn.Content.ToString();
+        }
+        private void bt_operationClicked(object sender, RoutedEventArgs e)
+        {
+            Button selectedBtn = (Button)sender;
+
+            var operation = selectedBtn.Content.ToString();
+
+            label_operations.Text += operation;
+
+            if (String.IsNullOrEmpty(currentOperation))
+            {
+                leftNumber = Decimal.Parse(label_result.Text);
+            }
+            else
+            {
+                rightNumber = Decimal.Parse(label_result.Text);
+
+                switch (currentOperation)
+                {
+                    case "+":
+                        leftNumber += rightNumber;
+                        break;
+                    case "-":
+                        leftNumber -= rightNumber;
+                        break;
+                    case "/":
+                        leftNumber /= rightNumber;
+                        break;
+                    case "*":
+                        leftNumber *= rightNumber;
+                        break;
+                    case "=":
+                        label_result.Text = leftNumber.ToString("F2");
+                        break;
+                    default:
+                        break;
+                }
+
+                label_result.Text = leftNumber.ToString("F2");
+                label_operations.Text = leftNumber.ToString("F2") + (operation == "=" ? ' ' : operation);
+            }
+
+            currentOperation = operation;
+            inputStatus = true;
+        }
+
+        private void bt_clearClicked(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            var typeClear = button.Content.ToString();
+
+            switch(typeClear)
+            {
+                case "CE":
+                    label_result.Text = String.Empty;
+                    break;
+                case "C":
+                    label_result.Text = String.Empty;
+                    label_operations.Text = String.Empty;
+                    currentOperation = String.Empty;
+                    break;
+                case "<":
+                    label_result.Text = label_result.Text.Substring(0, label_result.Text.Length - 1);
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
